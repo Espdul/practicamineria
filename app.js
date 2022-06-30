f9f436f998ca1c46c8b1fd8ddb495ed48f82c11e
Your Name <you@example.com>
2022-06-29 20:51:21 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const PORT = process.env.PORT || 3000;
+const app = express();
+const bodyParser = require('body-parser');
+
+//var router = express.Router();
+const  fetch = require('node-fetch');
+
+app.use(bodyParser.json());
+
+app.get('/verificar', async (req, res) => {
+    var response = await fetch('https://dog.ceo/api/breeds/image/random');
+    var data = await response.json();
+    return res.json(data);
+
+});
+
+app.listen(3000, function () {
+    console.log('La Aplicación está funcionando en el puerto 3000');
+});
